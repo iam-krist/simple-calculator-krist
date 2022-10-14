1e5f86b29921f7d642f7d21f35bcbff2e20ebe2c
krist <christiaanfourie60@gmail.com>
2022-10-14 17:43:02 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import  Calculator  from "./components/Calculator";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App-header flex justify-center items-stretch">
+        <img 
+          src='./logo.png' 
+          className="App-logo" 
+            alt="logo" />
+        <h1 className='
+          text-slate-400
+          leading-13
+          font-extralight
+          text-4xl'>Basic Calculator
+        </h1>                 
       </header>
+      <Calculator />
     </div>
   );
 }
